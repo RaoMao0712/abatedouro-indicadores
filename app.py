b89e6b8edb9f397276913582d2ff4b3bb346b27c
Thiago <thiagonascimento071282@gmail.com>
2026-05-27 08:51:20 -0400

    Edição em lote descartes

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# TESTE GIT
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
