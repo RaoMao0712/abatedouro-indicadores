5e8570d796bfb7c1aac74fc31936a8abef208903
Thiago <thiagonascimento071282@gmail.com>
2026-09-22 14:13:00 -0400

    feat: adicionar fundacao versionada de sku e roteiro

--- modules/engenharia_produtos/services.py
+++ modules/engenharia_produtos/services.py
@@ -5,6 +5,7 @@ from datetime import date
 from modules.almoxarifado.services import buscar_insumos_almoxarifado
 
 from . import repositories as repo
+from . import fundacao
 
 
 TIPOS_PRODUTO = {
@@ -29,6 +30,7 @@ STATUS_PRODUTO = {"Sim": "Ativo", "Não": "Inativo"}
 
 def criar_tabelas_engenharia_produtos():
     repo.criar_estrutura()
+    fundacao.criar_estrutura()
 
 
 def _texto(form, campo, obrigatorio=False):
